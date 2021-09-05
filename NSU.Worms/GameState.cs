0a321b9efeeec3fb51b30bdd2a67baa56ccf2399
penobulyator <dima.onishuk70@gmail.com>
2021-09-05 14:02:24 +0700

    Initial commit

---
+++ NSU.Worms/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NSU.Worms.Worm;
+using System.Linq;
+namespace NSU.Worms
+{
+    class GameState
+    {
+        private Dictionary<string, AbstactWorm> Worms { get; }
+        public override string ToString()
+        {
+            string wormsStr = String.Join(',', Worms.Values.ToList());
+            return $"Worms:[{wormsStr}]";
+        }
+    }
+}
---
+++ NSU.Worms/Worm/AbstactWorm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+namespace NSU.Worms.Worm
+{
+    abstract class AbstactWorm
+    {
+        private Point Coord;
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}({Coord.X},{Coord.Y})";
+        }
+        abstract public Direction AskToMove(GameState state);
+    }
+}
