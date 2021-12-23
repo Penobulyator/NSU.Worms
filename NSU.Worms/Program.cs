7b3959790e65435acb2b9ae8b137da3e230cd983
penobulyator <dima.onishuk70@gmail.com>
2021-12-23 22:07:27 +0700

    Remove drawing

---
+++ NSU.Worms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSU.Worms.Worm;
+namespace NSU.Worms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameState game = new GameState();
+            game.Worms = new List<AbstactWorm> {
+                new ClockwiseMovingWorm("bob", new System.Drawing.Point(5, 5)),
+                new ClockwiseMovingWorm("bib", new System.Drawing.Point(5, 5))
+            };
+
+            WormsSimulator wormsSimulator = new WormsSimulator(game);
+            wormsSimulator.Run((gameState) =>
+            {
+                Console.WriteLine(gameState.ToString());
+            });
+        }
+    }
+}
