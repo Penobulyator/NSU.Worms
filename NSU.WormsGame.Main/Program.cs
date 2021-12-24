9d8b86565a37da0dd43d4f0872d3223d28a880d5
penobulyator <dima.onishuk70@gmail.com>
2021-12-24 07:29:35 +0700

    Add main project

---
+++ NSU.WormsGame.Main/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using NSU.WormsGame.Services;
+using NSU.WormsGame.Services.WormActionGeneratorService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSU.WormsGame.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+
+        {
+            return Host.CreateDefaultBuilder(args)
+
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<WormsSimulatorService>();
+                    services.AddScoped<IFoodGeneratorService, FoodGeneratorService>();
+                    services.AddScoped<IWormNamesGeneratorService, WormNamesGeneratorService>();
+                    services.AddScoped<IWormActionGeneratorService, MoveTorawdsFoodWormActionGeneratorService>();
+                    services.AddScoped<IGameStateWriterService, GameStateWriterService>();
+                });
+
+        }
+
+    }
+}
+
